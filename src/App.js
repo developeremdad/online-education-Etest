940b44d52db8bab9793d6550b99b6d147e2a1b39
developeremdad <developeremdad@gmail.com>
2021-10-04 01:22:27 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-warning">this is app. js section</h1>
-      <Button variant="danger">Click Me</Button>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Router exact path="/">
+            <Home></Home>
+          </Router>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light bg-success">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <NavLink className="navbar-brand text-warning" to="/home">ONLINE TEST</NavLink>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active text-light" aria-current="page" to="/home">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-light" to="/services">Services</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-light" to="/about">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-light" to="/instructor">Instructor</NavLink>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Find Course" aria-label="Search" />
+                            <button className="btn btn-outline-warning" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
